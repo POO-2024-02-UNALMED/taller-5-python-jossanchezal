7a6a16a42b010e89f0cf346921008860a3839623
jossanchezal <jossanchezal@unal.edu.co>
2024-12-17 11:47:25 -0500

    18

--- zooAnimales/ave.py
+++ zooAnimales/ave.py
@@ -1,43 +1,52 @@
 from .animal import Animal
+
 class Ave(Animal):
-    _listado=[]
-    aguilas=0
-    halcones=0
+    _listado = []
+    halcones = 0
+    aguilas = 0
+    _colorPlumas = ''
+
     def __init__(self, nombre, edad, habitat, genero, colorPlumas):
-        super().__init__(nombre, edad, habitat, genero)
-        self._colorPlumas=colorPlumas
+        self._nombre = nombre
+        self._edad = edad
+        self._habitat = habitat
+        self._genero = genero
+        self._colorPlumas = colorPlumas
         Ave._listado.append(self)
-        
-    def cantidadAves():
-        cantidad=len(Ave._listado)
-        return cantidad
-    def movimiento():
-        return "volar"
-    def crearHalcon(self,nombre, edad, genero, zona):
-        self.colorPlumas="café glorioso"
-        self.habitat= "montañas"
-        self.nombre=nombre
-        self.edad=edad
-        self.genero=genero
-        self.zona=zona
-        Ave.halcones+=1
-    def crearAguila(self,nombre, edad, genero, zona):
-        self.nombre=nombre
-        self.edad=edad
-        self.genero=genero
-        self.zona=zona
-        Ave.aguilas+=1
-        self.colorPlumas="blanco y amarillo"
-        self.habitat="montañas"
-    def setNombre(self,nombre):
-        self._nombre=nombre
-    def getListado(self):
-        return Ave._listado
-    def setListado(self,listado):
-        Ave._listado=listado
+        Animal.setTotalAnimales(Animal.getTotalAnimales() + 1)
+
+    @classmethod
+    def getListado(cls):
+        return cls._listado
+    
+    def setListado(self, L: list) -> None:
+        self._listado = L
+
     def getColorPlumas(self):
         return self._colorPlumas
-    def setColorPlumas(self,color):
-        self._colorPlumaso=color
-def toString(self):
+    
+    def setColorPlumas(self, pe: bool) -> None:
+        self._colorPlumas = pe
+
+    def cantidadAves(self) -> int:
+        return len(self._listado)
+
+    @classmethod
+    def crearHalcon(cls, nombre, edad, genero) -> Animal:
+        cls._colorPlumas = 'cafe glorioso'
+        cls.setHabitat(cls, 'montanas')
+        Ave.halcones += 1
+        return Ave(nombre, edad, cls.getHabitat(cls), genero, cls._colorPlumas)
+
+    @classmethod
+    def crearAguila(cls, nombre, edad, genero) -> Animal:
+        cls._colorPlumas = 'blanco y amarillo'
+        cls.setHabitat(cls, 'montanas')
+        Ave.aguilas += 1
+        return Ave(nombre, edad, cls.getHabitat(cls), genero, cls._colorPlumas)
+
+    def movimiento():
+        return 'volar'
+    
+    def toString(self):
         return f'Mi nombre es {self._nombre}, tengo una edad de {self._edad}, habito en {self._habitat} y mi genero es {self._genero}'
